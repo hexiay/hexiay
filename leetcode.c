324c4d8de15ab9254aec98ecde8aaa903509ab27
hexiay <hewanxiang@gmail.com>
2018-05-15 17:26:49 +0800

    Update leetcode.c
    501. Find Mode in Binary Search Tree

--- leetcode.c
+++ leetcode.c
@@ -10,3 +10,54 @@ int jumpFloorII(int number) {
         //通过移位计算2的次方
         return 1<<(number-1);        
 }
+
+/*
+501. Find Mode in Binary Search Tree
+
+Given a binary search tree (BST) with duplicates, find all the mode(s) (the most frequently occurred element) in the given BST.
+
+Assume a BST is defined as follows:
+
+The left subtree of a node contains only nodes with keys less than or equal to the node's key.
+The right subtree of a node contains only nodes with keys greater than or equal to the node's key.
+Both the left and right subtrees must also be binary search trees.
+ 
+For example:
+Given BST [1,null,2,2],
+
+   1
+    \
+     2
+    /
+   2
+ 
+return [2].
+
+Note: If a tree has more than one mode, you can return them in any order.
+
+Follow up: Could you do that without using any extra space? (Assume that the implicit stack space incurred due to recursion does not count). 
+
+这道题让我们求二分搜索树中的众数，这里定义的二分搜索树中左根右结点之间的关系是小于等于的，有些题目中是严格小于的，所以一定要看清题目要求。所谓的众数就是出现最多次的数字，可以有多个，那么这道题比较直接点思路就是利用一个哈希表来记录数字和其出现次数之前的映射，然后维护一个变量mx来记录当前最多的次数值，这样在遍历完树之后，根据这个mx值就能把对应的元素找出来。那么用这种方法的话就不需要用到二分搜索树的性质了，随意一种遍历方式都可以，下面我们来看递归的中序遍历的解法如下：
+*/
+
+class Solution {
+public:
+    vector<int> findMode(TreeNode* root) {
+        vector<int> res;
+        int mx = 0;
+        unordered_map<int, int> m; 
+        inorder(root, m, mx);
+        for (auto a : m) {
+            if (a.second == mx) {
+                res.push_back(a.first);
+            }
+        }
+        return res;
+    }
+    void inorder(TreeNode* node, unordered_map<int, int>& m, int& mx) {
+        if (!node) return;
+        inorder(node->left, m, mx);
+        mx = max(mx, ++m[node->val]);
+        inorder(node->right, m, mx);
+    }
+};
