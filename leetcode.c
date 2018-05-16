2eb51a71c797c2b64f7ddab92b512d66a710cbe8
hexiay <hewanxiang@gmail.com>
2018-05-16 16:33:39 +0800

    Update leetcode.c

--- leetcode.c
+++ leetcode.c
@@ -162,3 +162,32 @@ public:
         return res;
     }
 };
+/*
+504. Base 7
+Given an integer, return its base 7 string representation.
+
+Example 1:
+Input: 100
+Output: "202"
+Example 2:
+Input: -7
+Output: "-10"
+Note: The input will be in range of [-1e7, 1e7].
+*/
+class Solution {
+public:
+    string convertToBase7(int num) {
+        if (num == 0)
+            return "0";
+        string res;
+        bool positive = (num > 0);
+        while (num != 0) {
+            res = to_string(abs(num) % 7) + res;
+            num = num / 7;
+        }
+        if (positive)
+            return res;
+        else
+            return "-" + res;
+    }
+};
