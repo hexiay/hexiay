f391b71ea2e52ba2bdc9b062afb672e3846e9830
hexiay <hewanxiang@gmail.com>
2018-05-17 12:38:14 +0800

    Update leetcode.c

--- leetcode.c
+++ leetcode.c
@@ -248,3 +248,46 @@ public:
         return (res == INT_MAX) ? -1 : res;
     }
 };
+/*
+506. Relative Ranks
+Given scores of N athletes, find their relative ranks and the people with the top three highest scores, who will be awarded medals: "Gold Medal", "Silver Medal" and "Bronze Medal".
+
+Example 1:
+Input: [5, 4, 3, 2, 1]
+Output: ["Gold Medal", "Silver Medal", "Bronze Medal", "4", "5"]
+Explanation: The first three athletes got the top three highest scores, so they got "Gold Medal", "Silver Medal" and "Bronze Medal". 
+For the left two athletes, you just need to output their relative ranks according to their scores.
+Note:
+N is a positive integer and won't exceed 10,000.
+All the scores of athletes are guaranteed to be unique.
+*/
+class Solution {
+public:
+    vector<string> findRelativeRanks(vector<int>& nums) {
+        int n = nums.size(), cnt = 1;
+        vector<string> res(n, "");
+        map<int, int> m;
+        for (int i = 0; i < n; ++i) {
+            m[nums[i]] = i;
+        }
+        for (auto it = m.rbegin(); it != m.rend(); ++it) {
+            if (cnt == 1) res[it->second] = "Gold Medal";
+            else if (cnt == 2) res[it->second] = "Silver Medal";
+            else if (cnt == 3) res[it->second] = "Bronze Medal";
+            else res[it->second] = to_string(cnt);
+            ++cnt;
+        }
+        return res;
+    }
+};
+/*
+507. Perfect Number
+We define the Perfect Number is a positive integer that is equal to the sum of all its positive divisors except itself.
+
+Now, given an integer n, write a function that returns true when it is a perfect number and false when it is not.
+Example:
+Input: 28
+Output: True
+Explanation: 28 = 1 + 2 + 4 + 7 + 14
+Note: The input number n will not exceed 100,000,000. (1e8)
+*/
