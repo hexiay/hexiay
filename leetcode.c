47281550e29e536071d19423d0e5504025658700
hexiay <hewanxiang@gmail.com>
2018-05-21 17:01:07 +0800

    Update leetcode.c

--- leetcode.c
+++ leetcode.c
@@ -557,4 +557,69 @@ Explanation: there are four ways to make up the amount:
 5=2+2+1
 5=2+1+1+1
 5=1+1+1+1+1
+
+This is a classic knapsack problem. Honestly, I'm not good at knapsack problem, it's really tough for me.
+dp[i][j] : the number of combinations to make up amount j by using the first i types of coins
+State transition:
+not using the ith coin, only using the first i-1 coins to make up amount j, then we have dp[i-1][j] ways.
+using the ith coin, since we can use unlimited same coin, we need to know how many way to make up amount j - coins[i] by using first i coins(including ith), which is dp[i][j-coins[i]]
+Initialization: dp[i][0] = 1
+Now we can see that dp[i][j] only rely on dp[i-1][j] and dp[i][j-coins[i]], then we can optimize the space by only using one-dimension array.
+
+这道题是之前那道Coin Change的拓展，那道题问我们最少能用多少个硬币组成给定的钱数，而这道题问的是组成给定钱数总共有多少种不同的方法。那么我们还是要使用DP来做，首先我们来考虑最简单的情况，如果只有一个硬币的话，那么给定钱数的组成方式就最多有1种，就看此钱数能否整除该硬币值。那么当有两个硬币的话，那么组成某个钱数的方式就可能有多种，比如可能由每种硬币单独来组成，或者是两种硬币同时来组成。那么我们怎么量化呢，比如我们有两个硬币[1,2]，钱数为5，那么钱数的5的组成方法是可以看作两部分组成，一种是由硬币1单独组成，那么仅有一种情况(1+1+1+1+1)；另一种是由1和2共同组成，说明我们的组成方法中至少需要由一个2，所以此时我们先取出一个硬币2，那么我们只要拼出钱数为3即可，这个3还是可以用硬币1和2来拼，所以就相当于求由硬币[1,2]组成的钱数为3的总方法。是不是不太好理解，多想想。那么我们的需要一个二维的dp数组，其中dp[i][j]表示用前i个硬币组成钱数为j的不同组合方法，那么怎么算才不会重复，也不会漏掉呢？我们采用的方法是一个硬币一个硬币的增加，每增加一个硬币，都从1遍历到amount，对于遍历到的当前钱数j，组成方法就是不加上当前硬币的频发dp[i-1][j]，还要加上，去掉当前硬币值的钱数的组成方法，当然钱数j要大于当前硬币值，那么我们的递推公式也在上面的分析中得到了：
+
+dp[i][j] = dp[i - 1][j] + (j >= coins[i - 1] ? dp[i][j - coins[i - 1]] : 0)
+
+注意我们要初始化每行的第一个位置为0，参见代码如下：      
 */
+class Solution {
+public:
+    int change(int amount, vector<int>& coins) {
+        vector<vector<int>> dp(coins.size() + 1, vector<int>(amount + 1, 0));
+        dp[0][0] = 1;
+        for (int i = 1; i <= coins.size(); ++i) {
+            dp[i][0] = 1;
+            for (int j = 1; j <= amount; ++j) {
+                dp[i][j] = dp[i - 1][j] + (j >= coins[i - 1] ? dp[i][j - coins[i - 1]] : 0);
+            }
+        }
+        return dp[coins.size()][amount];
+    }
+};
+/*
+我们可以对空间进行优化，我们发现dp[i][j]仅仅依赖于dp[i - 1][j] 和 dp[i][j - coins[i - 1]] 这两项，那么我们可以使用一个一维dp数组来代替，此时的dp[i]表示组成钱数i的不同方法。其实最开始的时候，博主就想着用一维的dp数组来写，但是博主开始想的方法是把里面两个for循环调换了一个位置，结果计算的种类数要大于正确答案，所以一定要注意for循环的顺序不能搞反，参见代码如下：
+*/
+class Solution {
+public:
+    int change(int amount, vector<int>& coins) {
+        vector<int> dp(amount + 1, 0);
+        dp[0] = 1;
+        for (int coin : coins) {
+            for (int i = coin; i <= amount; ++i) {
+                dp[i] += dp[i - coin];
+            }
+        }
+        return dp[amount];
+    }
+};
+class Solution {
+public:
+    int change(int amount, vector<int>& coins) {
+        if (amount == 0) return 1;
+        if (coins.empty()) return 0;
+        map<pair<int, int>, int> memo;
+        return helper(amount, coins, 0, memo);
+    }
+    int helper(int amount, vector<int>& coins, int idx, map<pair<int, int>, int>& memo) {
+        if (amount == 0) return 1;
+        else if (idx >= coins.size()) return 0;
+        else if (idx == coins.size() - 1) return amount % coins[idx] == 0;
+        if (memo.count({amount, idx})) return memo[{amount, idx}];
+        int val = coins[idx], res = 0;
+        for (int i = 0; i * val <= amount; ++i) {
+            int rem = amount - i * val;
+            res += helper(rem, coins, idx + 1, memo);
+        }
+        return memo[{amount, idx}] = res;
+    }
+};
