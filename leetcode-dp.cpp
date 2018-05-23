ad2c5f483dbd72583b4504427c32acded8b7d85d
hexiay <hewanxiang@gmail.com>
2018-05-23 16:56:12 +0800

    Update leetcode-dp.cpp

--- leetcode-dp.cpp
+++ leetcode-dp.cpp
@@ -173,13 +173,132 @@ public:
     }
 };
 /* =================================================================
+63. Unique Paths II
+A robot is located at the top-left corner of a m x n grid (marked 'Start' in the diagram below).
+The robot can only move either down or right at any point in time. The robot is trying to reach the bottom-right corner of the grid (marked 'Finish' in the diagram below).
+Now consider if some obstacles are added to the grids. How many unique paths would there be?
+An obstacle and empty space is marked as 1 and 0 respectively in the grid.
+Note: m and n will be at most 100.
+Example 1:
+Input:
+[
+  [0,0,0],
+  [0,1,0],
+  [0,0,0]
+]
+Output: 2
+Explanation:
+There is one obstacle in the middle of the 3x3 grid above.
+There are two ways to reach the bottom-right corner:
+1. Right -> Right -> Down -> Down
+2. Down -> Down -> Right -> Right
 ==================================================================== */
+class Solution {
+public:
+    int uniquePathsWithObstacles(vector<vector<int> > &obstacleGrid) {
+        if (obstacleGrid.empty() || obstacleGrid[0].empty()) return 0;
+        int m = obstacleGrid.size(), n = obstacleGrid[0].size();
+        if (obstacleGrid[0][0] == 1) return 0;
+        vector<int> dp(n, 0);
+        dp[0] = 1;
+        for (int i = 0; i < m; ++i) {
+            for (int j = 0; j < n; ++j) {
+                if (obstacleGrid[i][j] == 1) dp[j] = 0;
+                else if (j > 0) dp[j] += dp[j - 1];
+            }
+        }
+        return dp[n - 1];
+    }
+};
 /* =================================================================
+64. Minimum Path Sum
+Given a m x n grid filled with non-negative numbers, find a path from top left to bottom right which minimizes the sum of all numbers along its path.
+Note: You can only move either down or right at any point in time.
+Input:
+[
+  [1,3,1],
+  [1,5,1],
+  [4,2,1]
+]
+Output: 7
 ==================================================================== */
+class Solution {
+public:
+    int minPathSum(vector<vector<int>>& grid) {
+        if (grid.empty() || grid[0].empty())
+            return 0;
+        int m = grid.size();
+        int n = grid[0].size();
+        
+        vector<int> dp(n, 0);
+        for (int i = 0; i < m; ++i) {
+            for (int j = 0; j < n; ++j) {
+                if (i == 0) {
+                    dp[j] = grid[i][j] + ((j == 0) ? 0 : dp[j-1]);
+                } else {
+                    dp[j] = grid[i][j] + ((j == 0) ? dp[j] : min(dp[j], dp[j-1]));   
+                }
+            }
+        }
+        return dp[n-1];
+    }
+};
 /* =================================================================
+70. Climbing Stairs
+You are climbing a stair case. It takes n steps to reach to the top.
+
+Each time you can either climb 1 or 2 steps. In how many distinct ways can you climb to the top?
+
+Note: Given n will be a positive integer.
+
+Example 1:
+
+Input: 2
+Output: 2
+Explanation: There are two ways to climb to the top.
+1. 1 step + 1 step
+2. 2 steps
 ==================================================================== */
+class Solution {
+public:
+    int climbStairs(int n) {
+        if (n <= 1) return 1;
+        vector<int> dp(n);
+        dp[0] = 1; dp[1] = 2;
+        for (int i = 2; i < n; ++i) {
+            dp[i] = dp[i - 1] + dp[i - 2];
+        }
+        return dp.back();
+    }
+};
 /* =================================================================
+72. Edit Distance
+Given two words word1 and word2, find the minimum number of operations required to convert word1 to word2.
+
+You have the following 3 operations permitted on a word:
+
+    Insert a character
+    Delete a character
+    Replace a character
+
+Example 1:
+
+Input: word1 = "horse", word2 = "ros"
+Output: 3
+Explanation: 
+horse -> rorse (replace 'h' with 'r')
+rorse -> rose (remove 'r')
+rose -> ros (remove 'e')
+维护一个二维的数组dp，其中dp[i][j]表示从word1的前i个字符转换到word2的前j个字符所需要的步骤。那我们可以先给这个二维数组dp的第一行第一列赋值，这个很简单，因为第一行和第一列对应的总有一个字符串是空串，于是转换步骤完全是另一个字符串的长度。跟以往的DP题目类似，难点还是在于找出递推式，我们可以举个例子来看，比如word1是“bbc"，word2是”abcd“，那么我们可以得到dp数组如下：
+  Ø a b c d
+Ø 0 1 2 3 4
+b 1 1 1 2 3
+b 2 2 1 2 3
+c 3 3 2 1 2
+
+
 ==================================================================== */
+
 /* =================================================================
 ==================================================================== */
 /* =================================================================
